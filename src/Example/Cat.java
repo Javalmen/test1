3094993d583e01e2006d2ce7883490fa142a0b9d
Alexandr Menyaylov <new.mnlv1@gmail.com>
2021-12-26 15:45:44 +0300

    First commit

---
+++ src/Example/Cat.java
@@ -0,0 +1,64 @@
+package Example;
+
+public class Cat {
+    private double weight;
+    private String name;
+    private int age;
+    private String color;
+    private static int catCount;
+
+    public Cat() {
+        this(3000, "Barsik", age, color = "Grey");
+    }
+
+    public Cat(double weight, String name, int age, String color) {
+        this.weight = weight;
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        catCount++;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > this.age)
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+        meow(); meow(); meow();
+    }
+    public void meow() {
+        System.out.println("Meow");
+    }
+    private void pee() {
+        weight -= 20;
+    }
+    public void eat() {
+        weight += 50;
+        if (50 / weight > .01)
+            pee();
+    }
+}
+
+
---
+++ src/MyCode1/HelloWorld.java
@@ -0,0 +1,14 @@
+package MyCode1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int i = 45;
+        {
+        i++;
+    }
+            System.out.println("Value i = " + i);
+
+
+
+    }
+}
